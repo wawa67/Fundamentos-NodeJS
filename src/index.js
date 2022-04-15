72d87d21306fba6d65b05b68e5a1b24cc970cc4b
Wallace de Souza <wallace.silvadesouza67@gmail.com>
2022-04-14 23:38:17 -0300

    update for add new method http

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({message:"Hello World! I'm Ignite"});
+app.get("/courses", (request, response) => {
+    return response.send(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
-app.listen(3333);
+app.post("/courses",(request, response) => {
+    return response.send(["Curso 1","Curso 2","Curso 3","Curso 4"]);
+})
+
+app.put("/courses/:id",(request, response) => {
+    return response.send(["Curso 6","Curso 2","Curso 3","Curso 4"])
+})
+
+app.path("/courses/:id",(request, response) => {
+    return response.send(["Curso 6","Curso 7","Curso 3","Curso 4"])
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.send(["Curso 6","Curso 7","Curso 4"])
+})
+app.listen(3333)
