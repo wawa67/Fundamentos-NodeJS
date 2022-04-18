4e34ae1149c130148cf13d370e1471cf2ab6f8d0
Wallace de Souza <wallace.silvadesouza67@gmail.com>
2022-04-18 07:44:51 -0300

    update in the index and add requisition insomnia

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ const express = require("express");
 const app = express();
 
 app.get("/courses", (request, response) => {
-    return response.send(["Curso 1", "Curso 2", "Curso 3"]);
+    return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post("/courses",(request, response) => {
-    return response.send(["Curso 1","Curso 2","Curso 3","Curso 4"]);
+    return response.json(["Curso 1","Curso 2","Curso 3","Curso 4"]);
 })
 
 app.put("/courses/:id",(request, response) => {
-    return response.send(["Curso 6","Curso 2","Curso 3","Curso 4"])
+    return response.json(["Curso 6","Curso 2","Curso 3","Curso 4"])
 })
 
-app.path("/courses/:id",(request, response) => {
-    return response.send(["Curso 6","Curso 7","Curso 3","Curso 4"])
+app.patch("/courses/:id",(request, response) => {
+    return response.json(["Curso 6","Curso 7","Curso 3","Curso 4"])
 })
 
 app.delete("/courses/:id", (request, response) => {
-    return response.send(["Curso 6","Curso 7","Curso 4"])
+    return response.json(["Curso 6","Curso 7","Curso 4"])
 })
 app.listen(3333)
